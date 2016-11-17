2bc60251fac342a746c9b4ede02acacfbd41f665
lahtachev <lahtachev@gmail.com>
2016-11-17 14:39:05 +0400

    first commit

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -2,12 +2,22 @@
 
     namespace Lacodda\BxModule;
 
-    use Composer\Script\CommandEvent;
+    use Composer\Script\Event;
 
     class ScriptHandler
     {
         public static function copy (Event $event)
         {
+            // wet get ALL installed packages
+            $packages = $event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages();
+
+            $installationManager = $event->getComposer()->getInstallationManager();
+
+            foreach ($packages as $package) {
+                $installPath = $installationManager->getInstallPath($package);
+                //do my process here
+            }
+
             $event->getIO ()->write ("Show me after INSTALL/UPDATE command");
             $event->getIO ()->write ($_SERVER['DOCUMENT_ROOT']);
         }
