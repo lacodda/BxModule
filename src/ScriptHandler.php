0f64833532ffff7f2af6d18f97417160bda7db97
lahtachev <lahtachev@gmail.com>
2016-11-17 14:57:33 +0400

    first commit

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -4,18 +4,26 @@
 
     use Composer\Script\Event;
 
+    /**
+     * Class ScriptHandler
+     *
+     * @package Lacodda\BxModule
+     */
     class ScriptHandler
     {
+        /**
+         * @param Event $event
+         */
         public static function copy (Event $event)
         {
-            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/debug.log', print_r(34234, true));
             // wet get ALL installed packages
-            $packages = $event->getComposer()->getRepositoryManager()->getLocalRepository()->getPackages();
+            $packages = $event->getComposer ()->getRepositoryManager ()->getLocalRepository ()->getPackages ();
 
-            $installationManager = $event->getComposer()->getInstallationManager();
+            $installationManager = $event->getComposer ()->getInstallationManager ();
 
-            foreach ($packages as $package) {
-                $installPath = $installationManager->getInstallPath($package);
+            foreach ($packages as $package)
+            {
+                $installPath = $installationManager->getInstallPath ($package);
                 //do my process here
             }
 
